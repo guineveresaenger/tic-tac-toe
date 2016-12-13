1723fde61b0e63d90ab7f723416e4c63662ef40e
guineveresaenger <guineveresaenger@yahoo.com>
2016-12-13 14:51:08 -0800

    Added checkRows to Board to check if the rows have a winning condition

--- spec/board.spec.js
+++ spec/board.spec.js
@@ -14,4 +14,24 @@ describe('Board', function() {
     });
 
   });
+
+  xdescribe('checkWin', function() {
+    it ('checks the state of the board for a win', function() {
+
+    });
+
+  });
+
+  describe('checkRows', function() {
+    it ('checks the rows on the board for a win', function() {
+      expect(testBoard.checkRows()).toEqual(false);
+      testBoard.state = [['X', 'X', 'X'], [null, null, null], [null, null, null]];
+      expect(testBoard.checkRows()).toEqual(true);
+      testBoard.state = [[null, null, null], [null, null, null], ["O", "O", "O"]];
+      expect(testBoard.checkRows()).toEqual(true);
+      
+
+    });
+
+  });
 });
--- src/board.js
+++ src/board.js
@@ -2,6 +2,24 @@ var Board = function() {
   this.state = [[null, null, null], [null, null, null], [null, null, null]];
   this.win = false;
 
+
+
+};
+
+Board.prototype.checkRows = function() {
+  for(var i = 0; i < this.state.length; i++) {
+    var checkMark = this.state[i][0];
+    var counter = 0;
+    for(var j = 0; j < this.state[i].length; j++) {
+      if (this.state[i][j] == checkMark && checkMark !== null){
+        counter ++;
+      }
+    }
+    if (counter == 3) {
+      return true;
+    }
+  }
+  return false;
 };
 
 export default Board;
