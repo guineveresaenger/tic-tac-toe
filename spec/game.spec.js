6e52d80fffd934bc022d2b888a4ce6033e848d9c
Allison Hoke <allisonhoke@gmail.com>
2016-12-13 14:14:31 -0800

    created initial attributes of turnCounter, 2 players, and current player for game

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -1,9 +1,23 @@
 import Game from 'game';
+import Player from 'player';
 
-xdescribe('Game', function() {
-  describe('helloWorld', function(){
-    it ("should return Hello World", function(){
-      expect(Game.helloWorld()).toEqual('hello world!');
+describe('Game', function() {
+  var testGame = new Game();
+  describe('initial attributes', function(){
+    it ("turn counter should have an initial value of 0", function(){
+      expect(testGame.turnCounter).toEqual(0);
+    });
+
+    it ('has a playerOne', function() {
+      expect(testGame.playerOne instanceof(Player)).toEqual(true);
+    });
+
+    it ('has a playerTwo', function() {
+      expect(testGame.playerTwo instanceof(Player)).toEqual(true);
+    });
+
+    it ('has a currentPlayer', function() {
+      expect(testGame.currentPlayer).toEqual(testGame.playerOne);
     });
 
   });
--- src/game.js
+++ src/game.js
@@ -1,5 +1,10 @@
-var Game = function() {
+import Player from 'player';
 
+var Game = function() {
+  this.turnCounter = 0;
+  this.playerOne = new Player();
+  this.playerTwo = new Player();
+  this.currentPlayer = this.playerOne;
 };
 
 export default Game;
