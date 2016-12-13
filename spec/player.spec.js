4f8e76666a5021766a43fba0c8a9698a67affd4e
Allison Hoke <allisonhoke@gmail.com>
2016-12-13 13:55:03 -0800

    created attributes for player and created functions to set the attributes

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -1,10 +1,42 @@
 import Player from 'player';
 
-xdescribe('Player', function() {
-  describe('helloWorld', function(){
-    it ("should return Hello World", function(){
-      expect(Player.helloWorld()).toEqual('hello world!');
+describe('Player', function() {
+  var testPlayer = new Player();
+  describe('initial attributes', function(){
+    it ("should have a name attribute of `null` initially", function(){
+      expect(testPlayer.name).toEqual(null);
     });
-
+    it ('should have a mark initally set to `null`', function() {
+      expect(testPlayer.mark).toEqual(null);
+    });
+    it ('should have a turn attribute of false', function() {
+      expect(testPlayer.turn).toEqual(false);
+    });
   });
+
+  describe('setName', function() {
+    it ('should set the name', function() {
+      testPlayer.setName("Allison");
+      expect(testPlayer.name).toEqual("Allison");
+    });
+  });
+
+  describe('setMark', function() {
+    it ('should set the mark', function() {
+      testPlayer.setMark("X");
+      expect(testPlayer.mark).toEqual("X");
+    });
+  });
+
+  describe('toggle turn true/false', function() {
+    it ('should set the turn to true', function() {
+      testPlayer.setTurnTrue();
+      expect(testPlayer.turn).toEqual(true);
+    });
+    it ('should set the turn to false', function() {
+      testPlayer.setTurnFalse();
+      expect(testPlayer.turn).toEqual(false);
+    });
+  });
+
 });
--- src/player.js
+++ src/player.js
@@ -1,5 +1,24 @@
 var Player = function() {
-
+  this.name = null;
+  this.mark = null;
+  this.turn = false;
 };
+
+Player.prototype.setName = function(name) {
+  this.name = name;
+};
+
+Player.prototype.setMark = function(mark) {
+  this.mark = mark;
+};
+
+Player.prototype.setTurnFalse = function() {
+  this.turn = false;
+};
+
+Player.prototype.setTurnTrue = function() {
+  this.turn = true;
+};
+
 
 export default Player;
